56f56fceee5a13381af467e61b18506968acecf2
hanzohd <73936655+hanzohd@users.noreply.github.com>
2020-11-13 14:10:18 +0300

    hack-dark

---
+++ hack.py
@@ -0,0 +1,132 @@
+import os
+os.system("clear")
+print(" _   _        ___  __    __  ______  _____")
+print("| | / /      /   | \ \  / / |___  / /  _  \ ")
+print("| |/ /      / /| |  \ \/ /     / /  | | | |")
+print("| |\ \     / / | |   \  /     / /   | | | |")
+print("| | \ \   / /  | |   / /     / /__  | |_| |")
+print("|_|  \_\ /_/   |_|  /_/     /_____| \_____/")
+print("hello")
+print("welcome my friends")
+dgree = input ("what's your name: ")
+print("1 for download sqlm")
+print("2 for download Striker")
+print("3 for download goodlist")
+print("4 for download T-Host")
+print("5 for download Guardn")
+print("6 for download Mob-Droid")
+print("7 for download Tbomb")
+print("8 for download hunner")
+print("9 for download hummer")
+print("10 for download Tool-X")
+print("11 for download DarkPremium")
+print("12 for download shellphish")
+print("13 for download instahack")
+print("14 for download mbf")
+print("15 for download Card-Number")
+print("16 for download TOOLSINSTALLERv3")
+print("17 for download HxWhatsApp")
+print("18 for download mrx-passwords")
+print("19 for download MRKing")
+print("20 for download inshackle")
+print("21 for download hammer")
+print("22 for download CamPhish")
+print("23 for download Amer")
+print("24 for download DeadTrap")
+print("25 for download zphisher")
+print("26 for download king-hacking")
+print("27 for download blackeye")
+print("28 for download Ledear-Hacking")
+print("29 for download viren")
+print("30 for download FbYOchecker")
+print("31 for download Lazymux")
+print("32 for download hulk")
+print("33 for download PhoneSploit")
+print("34 for download TOOLSINS")
+print("35 for download IP-FY")
+print("36 for download Lazyscript")
+print("37 for download Report")
+print("38 for download V7x-Tool")
+print("39 for download group-hack")
+print("40 for download XerXes")
+X = input("type number: ")
+if X == "1":
+    os.system("git clone https://github.com/sqlmapproject/sqlm")
+if x == "2":
+    os.system("git clone https://github.com/s0md3v/Striker.git")
+if x == "3":
+    os.system("git clone https://github.com/joker0811/goodlist")
+if x == "4":
+    os.system("git clone https://github.com/Bhai4You/T-Host")
+if x == "5":
+    os.system("git clone https://github.com/methaza/guardn")
+if x == "6":
+    os.system("git clone https://github.com/kinghacker0/Mob-Droid")
+if x == "7":
+    os.system("git clone https://github.com/TheSpeedX/TBomb")
+if x == "8":
+    os.system("git clone https://github.com/b3-v3r/Hunner")
+if x == "9":
+    os.system("git clone https://github.com/cyweb/hammer")
+if x == "10":
+    os.system("git clone https://github.com/rajkumardusad/Tool-X")
+if x == "11":
+    os.system("git clone https://github.com/TheMagizz/DarkPremium")
+if x == "12":
+    os.system("gitclone https://github.com/thelinuxchoice/shellphish")
+if x == "13":
+    os.system("git clone https://github.com/fuck3erboy/instahack")
+if x == "14":
+    os.system("git clone https://githup.com/pirmansx/mbf")
+if x == "15":
+    os.system("git clone https://github.com/INDOnimous/Card-Number")
+if x == "16":
+    os.system("git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3")
+if x == "17":
+    os.system("git clone https://github.com/Bl4ckDr460n/HxWhatsApp")    
+if x == "18":
+    os.system("git clone https://github.com/bbb647/mrx-passwords")
+if x == "19":
+    os.system("git clone https://github.com/king-hacking/MRKING")
+if x == "20":
+    os.system("git clone https://github.com/thelinuxchoice/inshackle.git")
+if x == "21":
+    os.system("git clone https://github.com/cyweb/hammer")
+if x == "22":
+    os.system("git clone https://github.com/techchipnet/CamPhish")
+if x == "23":
+    os.system("git clone https://github.com/amerlaceset/Amer")
+if x == "24":
+    os.system("git clone https://github.com/Chr0m0s0m3s/DeadTrap")
+if x == "25":
+   os.system("git clone https://github.com/htr-tech/zphisher")
+if x == "26":
+    os.system("git clone https://github.com/king-hacking")
+if x == "27":
+    os.system("git clone https://github.com/An0nUD4Y/blackeye")
+if x == "28":
+    os.system("git clone https://github.com/Ledear-Hacker/LEDEAR_HACKING")
+if x == "29":
+    os.system("git clone https://github.com/tealpartners/viren")
+if x == "30":
+    os.system("git clone https://github.com/EmadMemo/FbYOchecker")
+if x == "31":
+    os.system("git clone https://github.com/Gameye98/Lazymux")
+if x == "32":
+    os.system("git clone https://github.com/grafov/hulk")
+if x == "33":
+    os.system("git clone https://github.com/mishabzzz/PhoneSploit")
+if x == "34":
+    os.system("git clone https://github.com/TUANB4DUT/TOOLSINS")
+if x == "35":
+    os.system("git clone https://github.com/T4P4N/IP-FY")
+if x == "36":
+    os.system("git clone https://github.com/TechnicalMujeeb/Termux-Lazyscript.git")
+if x == "37":
+    os.system("git clone https://github.com/IlayTamvan/Report")
+if x == "38":
+    os.system("git clone https://github.com/Vairous7x/V7x-Tool")
+if x == "39":
+    os.system("git clone https://github.com/Mr-NemrMedo/fb-group-hack")
+if x == "40":
+    os.system("git clone https://github.com/CyberXCodder/XerXes.git")
